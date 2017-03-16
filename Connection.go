41a7792cd49aedb780bc6e4d8f83447bf3c0f401
Elliot Sperling <elliotsperling@Elliots-MacBook-Pro.local>
2017-03-16 13:34:39 -0700

    Add retries and state change

--- Connection.go
+++ Connection.go
@@ -1,21 +1,33 @@
 package go_networking
 
 import (
+	"bytes"
 	"encoding/base64"
 	"git.cyberdust.com/radicalapp/go-networking/logger"
 	"io/ioutil"
-	"time"
 	"net/http"
-	"io"
-	"bytes"
+	"time"
 )
 
 // Set up logging
 var log = logger.Register("git.cyberdust.com/radicalapp/go-networking")
 
-const http_get_method string = "GET"
-const http_post_method string = "POST"
-const http_upload_method string = "UPLOAD"
+const (
+	http_get_method    string = "GET"
+	http_post_method   string = "POST"
+	http_upload_method string = "UPLOAD"
+)
+
+type ConnectionState int
+
+const (
+	CONNECTION_STATE_DISCONNECTED ConnectionState = 0
+	CONNECTION_STATE_CONNECTING   ConnectionState = 1
+	CONNECTION_STATE_CONNECTED    ConnectionState = 2
+	// MAYBE IN FUTURE...?
+	//CONNECTION_STATE_RECONNECTING ConnectionState = 3
+	//CONNECTION_STATE_FAILED ConnectionState = 4
+)
 
 type Completion func([]byte, error)
 
@@ -25,28 +37,39 @@ type OnReceived func([]byte)
 type OnClosed func()
 type OnError func(err error)
 type OnProgress func(progress int)
+type OnStateChanged func(state ConnectionState)
 
 type basicAuthorization struct {
 	username string
 	password string
 }
 
+type IdealResponse interface {
+	getIdealResponseTemplate() interface{}
+}
+
 type Connection struct {
 	url              string
 	params           Params
 	timeoutInSeconds time.Duration
-
-	headers map[string]string
+	method           string
+	state            ConnectionState
+	numberOfRetries  int
+	headers          map[string]string
 
 	// Basic auth variables
 	basicAuthorization basicAuthorization
 
 	// Functions to handle connection states
-	OnStarted  OnStarted
-	OnReceived OnReceived
-	OnClosed   OnClosed
-	OnError    OnError
-	OnProgress OnProgress
+	OnStarted      OnStarted
+	OnReceived     OnReceived
+	OnClosed       OnClosed
+	OnError        OnError
+	OnProgress     OnProgress
+	OnStateChanged OnStateChanged
+	//
+	//idealResponse      IdealResponse
+	//guaranteeExecution bool
 }
 
 // NewConnection creates a new connection with the appropriate Dust auth headers.
@@ -78,33 +101,56 @@ func (c *Connection) PutHeader(key, value string) {
 	c.headers[key] = value
 }
 
+func (c *Connection) SetNumberOfRetries(number int) {
+	log.Debug("Setting number of retries: ", number)
+	c.numberOfRetries = number
+}
+
 func (c *Connection) GET(completion Completion) {
-	c.makeRequest(http_get_method, completion)
+	log.Debug("GET")
+	c.method = http_get_method
+	c.makeRequest(completion)
 }
 
 func (c *Connection) POST(completion Completion) {
-	c.makeRequest(http_post_method, completion)
+	c.method = http_post_method
+	c.makeRequest(completion)
 }
 
 func (c *Connection) UPLOAD(completion Completion) {
-	c.makeRequest(http_upload_method, completion)
+	c.method = http_upload_method
+	c.makeRequest(completion)
 }
 
-func (c *Connection) makeRequest(method string, completion Completion) {
-	c.OnStarted()
-	body := c.makeBody(method)
+func (c *Connection) makeRequest(completion Completion) {
+	//if c.guaranteeExecution {
+	//	if c.idealResponse == nil {
+	//		err := errors.New("Ideal response not configured")
+	//		c.processError(err, completion)
+	//		return
+	//	}
+	//}
+	if c.OnStarted != nil {
+		c.OnStarted()
+	}
+	c.changeState(CONNECTION_STATE_DISCONNECTED, CONNECTION_STATE_CONNECTING)
 
-	req, err := http.NewRequest(method, c.url, body)
-	c.makeParams(method, req)
+	//body := c.makeBody(method)
+
+	req, err := http.NewRequest(c.method, c.url, nil)
+	c.makeParams(req)
 	if err != nil {
-		c.OnError(err)
+		if c.OnError != nil {
+			c.OnError(err)
+		}
 		completion(nil, err)
 
 	} else {
 		// Set the headers of the request
 		if &c.basicAuthorization != nil {
 			encoded := base64.StdEncoding.EncodeToString([]byte(c.basicAuthorization.username + ":" + c.basicAuthorization.password))
-			c.PutHeader("Authorization", "Bearer " + encoded)
+			c.PutHeader("Authorization", "Basic "+encoded)
+
 		}
 		for key, val := range c.headers {
 			req.Header.Set(key, val)
@@ -113,8 +159,8 @@ func (c *Connection) makeRequest(method string, completion Completion) {
 	}
 }
 
-func (c *Connection) makeParams(method string, req *http.Request) {
-	if method != http_upload_method {
+func (c *Connection) makeParams(req *http.Request) {
+	if c.method != http_upload_method {
 		req.URL.RawQuery = c.params.urlEncodeValues()
 	}
 }
@@ -148,23 +194,58 @@ func (c *Connection) makeBody(method string) *bytes.Buffer {
 	return nil
 }
 
-func (c *Connection) doRequest(req *http.Request, completion Completion) ([]byte, error) {
+func (c *Connection) doRequest(req *http.Request, completion Completion) {
+	log.Debug("Doing request")
 	client := &http.Client{
 		Timeout: c.timeoutInSeconds * time.Second,
 	}
 
+	c.changeState(CONNECTION_STATE_CONNECTING, CONNECTION_STATE_CONNECTED)
 	response, err := client.Do(req)
-
 	defer response.Body.Close()
-	contents, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		log.Error(err)
-		c.OnError(err)
-		completion(nil, err)
-		return nil, err
+		c.processError(err, completion)
 	} else {
-		c.OnReceived(contents)
-		c.OnClosed()
-		return contents, nil
+		contents, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			c.processError(err, completion)
+		} else {
+			c.processResponse(contents, completion)
+		}
 	}
+}
+
+func (c *Connection) changeState(from, to ConnectionState) {
+	// MAYBE GUARANTEE CORRECT STATE CHANGE, IN FUTURE
+	log.Debug("Changing state from: ", from, " to: ", to)
+	c.state = to
+	if c.OnStateChanged != nil {
+		c.OnStateChanged(to)
+	}
+}
+
+func (c *Connection) processError(err error, completion Completion) {
+	c.changeState(CONNECTION_STATE_CONNECTED, CONNECTION_STATE_DISCONNECTED)
+	if c.numberOfRetries > 0 {
+		c.numberOfRetries--
+		c.makeRequest(completion)
+	} else {
+		log.Error(err)
+		if c.OnError != nil {
+			c.OnError(err)
+		}
+		completion(nil, err)
+	}
+}
+
+func (c *Connection) processResponse(response []byte, completion Completion) {
+	log.Debug("Processing response")
+	c.changeState(CONNECTION_STATE_CONNECTED, CONNECTION_STATE_DISCONNECTED)
+	if c.OnReceived != nil {
+		c.OnReceived(response)
+	}
+	if c.OnClosed != nil {
+		c.OnClosed()
+	}
+	completion(response, nil)
 }
