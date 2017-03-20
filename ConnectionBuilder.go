0fa4c33c339a4c6c7aad554f70d1543170e402e6
rohitkotian <rohit@cyberdust.com>
2017-03-20 12:21:49 -0700

    Add methods in builder

--- ConnectionBuilder.go
+++ ConnectionBuilder.go
@@ -1,17 +1,24 @@
 package go_networking
 
 type ConnectionBuilder struct {
-
-	connection *Connection
-
+	connection Connection
 }
 
-//func NewConnectionBuilder(urlString string, method)
-
-func (cb *ConnectionBuilder) setUrlString(urlString string) {
+func (cb *ConnectionBuilder) SetUrlString(urlString string) *ConnectionBuilder {
 	cb.connection.urlString = urlString
+	return cb
 }
 
-func (cb *ConnectionBuilder) build() *Connection {
+func (cb *ConnectionBuilder) SetParams(params Params) *ConnectionBuilder {
+	cb.connection.params = params
+	return cb
+}
+
+func (cb *ConnectionBuilder) SetMethod(method HTTP_METHOD) *ConnectionBuilder {
+	cb.connection.method = method
+	return cb
+}
+
+func (cb *ConnectionBuilder) Build() Connection {
 	return cb.connection
 }
